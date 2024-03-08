738a906e3a4d95b921e5273d41add194755e2881
João Anderson <joao.anderson.af@gmail.com>
2024-03-08 16:48:03 -0300

    refatorando para implementar closure e funcao de alta ardem

--- inputs.js
+++ inputs.js
@@ -1,30 +1,37 @@
-const createInput = (divId, labelId, i) => {
-    let div = document.getElementById(divId)
-    let label = document.getElementById(labelId)
+// todas as funções utilizadas são funções lambda
+const createInput = elementId => {
+    let div = document.getElementById(elementId)
     let inputDiv = document.createElement("div")
     let input = document.createElement("input")
-    label.hidden = false
     inputDiv.className = "col-md-2"
     input.type = "number"
     input.className = "form-control"
-    inputDiv.id = divId + i
     inputDiv.appendChild(input)
     div.appendChild(inputDiv)
 }
 
-const removeAllInpus = (elementId, labelId) => {
+const countArray = (value, indexAdd = 0) => new Array(parseInt(value) + indexAdd).fill(0)
+
+const hidden = (elementId, bool) => document.getElementById(elementId).hidden = bool
+
+const removeAllChildren = elementId => {
     const element = document.getElementById(elementId)
     element && [...element.children].forEach(child => child.remove())
-    document.getElementById(labelId).hidden = true
+}
+// função de alta ordem, pois retorna uma função como resultado
+// implementa closure retornando uma função interna que se utiliza de
+// variaveis do escopo da função externa
+const inputs = (elementId, labelId) => {
+    return element => {
+        removeAllChildren(elementId)
+        const exec = isNaN(element.value) || element.value == "" ?
+            () => hidden(labelId, true) :
+            () => {
+                hidden(labelId, false)
+                countArray(element.value, 1).forEach(() => createInput(elementId))
+            }
+        exec()
+    }
 }
 
-const inputIndexes = (element, elementId, labelId) => {
-    const exec = isNaN(element.value) || element.value == "" ?
-        () => removeAllInpus(elementId, labelId) :
-        () => {
-            removeAllInpus(elementId, labelId)
-            const count = new Array(parseInt(element.value) + 1).fill(0)
-            count.forEach((_, i) => createInput(elementId, labelId, i))
-        }
-    exec()
-}
+const inputIndexes = inputs('indexes', 'label-indexes')
